7d14e43fb11ab7a198b763112781a4fd7733b83e
Mark Fominykh <mark545154@mail.ru>
2021-12-13 18:46:46 +0700

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,52 @@
+$(function (){
+    $('.gallery__inner').magnificPopup({
+        delegate: 'a',
+        type: 'image',
+        tLoading: 'Loading image #%curr%...',
+        mainClass: 'mfp-img-mobile',
+        gallery: {
+            enabled: true,
+            navigateByImgClick: true,
+            preload: [0,1] // Will preload 0 - before current, and 1 after the current image
+        },
+    });
+});
+
+
+// // This our standard require js bootstrap file.  It assumes you are using the
+// // require-jquery.js file that require.js provides
+// // Set the require.js configuration for the application
+// require.config({
+//
+//     // Base path used to load scripts
+//     baseUrl: 'js/',
+//
+//     // Prevent caching during dev
+//     urlArgs: "bust=" + (new Date()).getTime(),
+//
+//     // Exclude these modules on build
+//     stubModules: ['text'],
+//
+//     // Set common library paths
+//     paths: {
+//         jquery: 'empty:', // jquery is already loaded
+//         underscore: 'libs/underscore',
+//         backbone: 'libs/backbone'
+//     }
+// });
+//
+// // Define the application entry point
+// define('main', function (require) {
+//
+//     // Dependencies
+//     var $ = require('jquery'),
+//         router = require('router'); // This is the router.js file.  Note, it gets invoked before DOM is loaded
+//
+//     // Auto go to a project detail page.  Trigger = true will tell it to run the callback
+//     // function from the router definition.
+//     router.navigate("project/some-slug", {trigger:true})
+//
+// });
+//
+// // Start the application
+// require(['main']);
